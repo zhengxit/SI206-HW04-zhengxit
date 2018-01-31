8132e08ad2d6f919c008df5a042299736c465f92
zhengxit <zhengxit@umich.edu>
2018-01-31 10:19:15 -0500

    modify questions

--- magic_eight.py
+++ magic_eight.py
@@ -1,2 +1,9 @@
 def user_input():
-    print("What is your question?")
+    current_input = input("What is your question?")
+    while (current_input != "quit"):
+        if current_input[-1] != "?":
+            print("I’m sorry, I can only answer questions.")
+        current_input = input("What is your question?")
+
+
+user_input()
