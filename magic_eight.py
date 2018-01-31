ab8b4a7456ef63c431960111bee9d7d58f6c3f5b
Daniel Graw <danielgraw@0587344622.wireless.umich.net>
2018-01-31 10:06:35 -0500

    pick an random answer

--- magic_eight.py
+++ magic_eight.py
@@ -1,2 +1,10 @@
+from random import randrange
+
 def user_input():
     print("What is your question?")
+
+def get_answer():
+    answers = ['It is certain', 'It is decidedly so', 'without a doubt', 'yes definitely', 'you may rely on it', 'as i see it yes', 'most likely', 'outlook good','yes','signs point to yes','reply hazy try again','ask again later','better not tell you now','cannot predict now','concentrate and ask again',"don't count on it",'my reply is no', 'my sources say no','outlook not so good','very doubtful']
+    return answers[randrange(len(answers))]
+
+print(get_answer())
